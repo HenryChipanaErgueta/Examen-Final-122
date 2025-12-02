8efd09e3a4b194f8e8619a56b531263da0183021
HenryDarioChipanaErgueta <@chipanahenry697@gmail.com>
2025-12-01 21:07:23 -0400

    esqueleto1

---
+++ src/Principal.jsx
@@ -0,0 +1,14 @@
+import "./Principal.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+function Pag0(props) {
+  return (
+    <div className="padre">
+      <div className="header">
+        <h1>Examen Final Henry Chipana Ergueta</h1>
+      </div>
+      <div className="menu">este es el menu</div>
+      <div className="contenido">este es el contenido de la pagina 0</div>
+      <div className="footer">este es el footer</div>
+    </div>
+  );
+}
